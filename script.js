1e075da4a8e5921ac29931b8cf8e9e7d5fdc3f37
pedrothecatholic <pedro.sousa21@outlook.com>
2023-12-12 07:59:24 -0300

    Return class statement from body to html

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleMode() {
-  const body = document.body;
-  body.classList.toggle('light');
+  const html = document.documentElement;
+  html.classList.toggle('light');
 
   // pegar a tag img
   const img = document.querySelector('#profile img');
 
   // substituir a imagem
-  if (body.classList.contains('light')) {
+  if (html.classList.contains('light')) {
     // se estiver light mode, adicionar a imagem light
     img.setAttribute('src', './assets/avatar-light.png');
   } else {
