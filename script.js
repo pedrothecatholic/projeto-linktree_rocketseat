039967296733452d448260bfe31eae9537b3f30b
pedrothecatholic <pedro.sousa21@outlook.com>
2023-12-12 08:50:05 -0300

    Applying dark mode persistency

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
+// function toggleMode() {
+//   const html = document.documentElement;
+//   html.classList.toggle('light');
+
+//   // pegar a tag img
+//   const img = document.querySelector('#profile img');
+
+//   // substituir a imagem
+//   if (html.classList.contains('light')) {
+//     // se estiver light mode, adicionar a imagem light
+//     img.setAttribute('src', './assets/avatar-light.png');
+//   } else {
+//     // se estiver sem light mode, manter a imagem normal
+//     img.setAttribute('src', './assets/avatar.png');
+//   }
+// }
+
 function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle('light');
 
-  // pegar a tag img
+  // Pegar a tag img
   const img = document.querySelector('#profile img');
 
-  // substituir a imagem
+  // Salvar o estado atual do modo no armazenamento local
+  const currentMode = html.classList.contains('light') ? 'light' : 'dark';
+  localStorage.setItem('theme', currentMode);
+
+  // Substituir a imagem
   if (html.classList.contains('light')) {
-    // se estiver light mode, adicionar a imagem light
+    // Se estiver no modo claro, adicionar a imagem light
     img.setAttribute('src', './assets/avatar-light.png');
   } else {
-    // se estiver sem light mode, manter a imagem normal
+    // Se estiver no modo escuro, manter a imagem normal
     img.setAttribute('src', './assets/avatar.png');
   }
 }
+
+// Aplicar o modo salvo ao carregar a página
+function applySavedTheme() {
+  const html = document.documentElement;
+  const img = document.querySelector('#profile img');
+  const savedTheme = localStorage.getItem('theme');
+
+  if (savedTheme) {
+    html.classList.add(savedTheme);
+
+    // Atualizar a imagem de acordo com o modo salvo
+    if (savedTheme === 'light') {
+      img.setAttribute('src', './assets/avatar-light.png');
+    }
+  }
+}
+
+// Aplicar o modo salvo ao carregar a página
+applySavedTheme();
